4e0a52512452ceb3c49e5b2c5dda764643fff920
Kayla-Morrell <kayla.morrell@roswellpark.org>
2020-12-17 15:34:39 -0500

    Successfully able to publish to bucket

--- R/publish_resource.R
+++ R/publish_resource.R
@@ -1,13 +1,13 @@
 #' A function that publishes resource to the hub S3 bucket
 #'
 #' This function uses functionality from the aws.s3 package to put files or 
-#' directorieso on the Bioconductor's test hub S3 bucket. The user should 
+#' empty directories on the Bioconductor's test hub S3 bucket. The user should 
 #' have already contacted the hubs maintainers at hubs@bioconductor.org to get 
 #' the necessary credentials to access the bucket. These credentials should be 
 #' delcared in the system environment prior to running this function.
 #'
-#' @param path A `character(1)` path to the file or directory to be added to 
-#'     the bucket.
+#' @param path A `character(1)` path to the file or the name of the empty 
+#'     directory to be added to the bucket.
 #' @param object A `character(1)` to indicate how the file should be named on 
 #'     the bucket.
 #'
@@ -17,17 +17,25 @@
 #' @export
 #'
 #' @examples
-#' fl <- tempdir()
-#'
+#' publish_resource("test_dir")
+#' tmp <- tempfile()
+#' utils::write.csv(mtcars, file = tmp)
+#' publish_resource(tmp, "test_dir/mtcars.csv")
 publish_resource <- function(path, object)
 {
     vars <- c("AWS_DEFAULT_OUTPUT", "AWS_DEFAULT_REGION", "AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY")
-    tryCatch(stopifnot(all(nzchar(Sys.getenv(vars)))),
-        error = stop("Not all system environments are set, do so and rerun function.")
-    )
+    if (!all(nzchar(Sys.getenv(vars))))
+        stop("Not all system environments are set, do so and rerun function.")
 
     if (is_file(path))
-        put_object(path, object, bucket = "AnnotationContributor", acl = "public-read") 
+        put_object(file = path, 
+            object = object,
+            bucket = "annotation-contributor",
+            acl = "public-read"
+        ) 
     else
-        put_folder(path, bucket = "AnnotationContributor") 
+        put_folder(folder = path,
+            bucket = "annotation-contributor",
+            acl = "public-read"
+        ) 
 }
