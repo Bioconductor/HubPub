94b058576689f6970b0ea7a47888588d9c2053bd
Kayla-Morrell <kayla.morrell@roswellpark.org>
2021-11-12 15:22:20 -0500

    Fix to create_pkg tests

--- tests/testthat/test_create_pkg.R
+++ tests/testthat/test_create_pkg.R
@@ -9,7 +9,11 @@ test_that("`create_pkg()` works",
     expect_identical(unname(dsc[,"Package"]), "tstAHPkg")
     expect_identical(unname(dsc[,"Version"]), "0.99.0")
     expect_identical(unname(dsc[,"biocViews"]), "AnnotationHub")
-    expect_identical(unname(dsc[,"Imports"]), "AnnotationHubData")
+    expect_identical(unname(dsc[,"Imports"]), "AnnotationHub")
+
+    sugs <- unname(dsc[,"Suggests"])
+    sugs_split <- unlist(strsplit(sugs, "\n"))
+    expect_identical(sugs_split[2], "AnnotationHubData")
 
     create_pkg(paste0(fl, "/tstEHPkg"), "ExperimentHub")
 
@@ -18,6 +22,10 @@ test_that("`create_pkg()` works",
     expect_identical(unname(dsc2[,"Version"]), "0.99.0")
     expect_identical(unname(dsc2[,"biocViews"]), "ExperimentHub")
     expect_identical(unname(dsc2[,"Imports"]), "ExperimentHub")
+
+    sugs2 <- unname(dsc2[,"Suggests"])
+    sugs2_split <- unlist(strsplit(sugs2, "\n"))
+    expect_identical(sugs2_split[2], "ExperimentHubData")
 
     expect_error(create_pkg())
     expect_error(create_pkg(paste0(fl, "/tstAHPkg")))
