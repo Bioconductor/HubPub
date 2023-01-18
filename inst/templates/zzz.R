72975a0caac218601b2876fe1195d41720791868
Charlotte Soneson <charlottesoneson@gmail.com>
2023-01-18 16:36:51 +0100

    libmane -> libname in .onLoad

--- inst/templates/zzz.R
+++ inst/templates/zzz.R
@@ -1,4 +1,4 @@
-.onLoad <- function(libmane, pkgname) {
+.onLoad <- function(libname, pkgname) {
     fl <- system.file("extdata", "metadata.csv", package=pkgname)
     titles <- read.csv(fl, stringsAsFactors=FALSE)$Title
     ExperimentHub::createHubAccessors(pkgname, titles)
